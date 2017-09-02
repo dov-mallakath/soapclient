fc4a7b41dfc29e71941dc0011f596a9b5f194eb2
Denys Ovcharuk <mallakath@gmail.com>
2017-09-02 20:31:26 +0300

    Homework lesson2 - completed without testing negative values

--- src/test/java/com/convertspeed/ConvertSpeedsTest.java
+++ src/test/java/com/convertspeed/ConvertSpeedsTest.java
@@ -1,6 +1,9 @@
 package com.convertspeed;
 
 import com.utils.FileOperations;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -18,6 +21,9 @@ public class ConvertSpeedsTest {
 
     private ConvertSpeedsClient convertSpeedsClient = new ConvertSpeedsClient();
     private ConvertSpeedsSoap convertSpeedsSoap = convertSpeedsClient.client;
+
+    private String failedReport = "";
+    private String testDataLog = "";
 
     private static HashSet inputDataCsvParser(String filePath){
         FileOperations fileOperations = new FileOperations();
@@ -51,6 +57,24 @@ public class ConvertSpeedsTest {
     @Test(dataProvider = "Calculations")
     public void getSpeed(HashMap data) {
         Double convertSpeed = convertSpeedsSoap.convertSpeed(Double.valueOf(data.get("speed").toString()),SpeedUnit.fromValue(data.get("from").toString()),SpeedUnit.fromValue(data.get("to").toString()));
+        testDataLog = data.get("from") + "," + data.get("to") + "," + data.get("speed") + "," + data.get("expectedResult") + "," + convertSpeed;
         assertEquals(Double.valueOf(data.get("expectedResult").toString()),convertSpeed);
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            if (failedReport.isEmpty()) {
+                failedReport = "from" + "," + "to" + "," + "speed" + "," + "expectedResult" + "," + "actualFailedResult" + System.getProperty("line.separator");
+            }
+            failedReport += testDataLog + System.getProperty("line.separator");
+        }
+    }
+
+    @AfterClass
+    public void afterClass() {
+        if (!failedReport.isEmpty()) {
+            System.out.println(failedReport);
+        }
     }
 }
